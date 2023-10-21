56d9b89eff027cc13b1874a49b1df85c7d6cc783
soniao2003 <soniao2003@gmail.com>
2023-10-22 00:52:43 +0200

    frontend

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Soni%40O2003@localhost/webapplication'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,10 +21,11 @@ class Product(db.Model):
       description = db.Column(db.String(255))
       isDeleted = db.Column(db.Boolean, default=False)
       creationDate = db.Column(db.DateTime, default = datetime.now)
-      creatorUser = db.Column(db.String(255))
+      creatorUser = db.Column(db.String(255), db.ForeignKey('user.login'), nullable=False)
       imageUrl = db.Column(db.String(255))
       categoryId = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
       categoryRel = db.relationship('Category', backref='products')
+      userRel = db.relationship('User', backref='products')
 
 
       #idcategory???
@@ -43,10 +45,16 @@ class Category(db.Model):
       productRel = db.relationship('Product', backref='category', lazy=True)
       #idproduct????
 
+class User(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     login = db.Column(db.String(255))
+     password = db.Column(db.String(255))
+     role = db.Column(db.Integer)
+     productRel = db.relationship('Product', backref='user', lazy=True)
+
 class ProductSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Product
-        fields = ('id', 'title', 'description', 'new_column1', 'new_column2', 'creatorUser', 'imageUrl', 'categoryId', 'categoryRel')
 
 class CommentSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -56,15 +64,26 @@ class CategorySchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Category
 
-product_schema = ProductSchema()
-product_schema = ProductSchema(many=True)
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
 
-#18:00 marshmallow
+product_schema = ProductSchema()
+product_schema_many = ProductSchema(many=True)
+
+user_schema = UserSchema()
+user_schema_many = UserSchema(many=True)
+
+category_schema = CategorySchema()
+category_schema_many = CategorySchema(many=True)
+
+comment_schema = CommentSchema()
+comment_schema_many = CommentSchema(many=True)
 
 @app.route('/get', methods = ['GET'])
 def get_products():
         all_products = Product.query.all()
-        products_results = product_schema.dump(all_products)
+        products_results = product_schema_many.dump(all_products)
         return jsonify(products_results)
 
 @app.route('/add', methods = ['POST'])
@@ -74,12 +93,15 @@ def add_product():
       #isDeleted = request.json['isDeleted']
       creatorUser = request.json['creatorUser']
       imageUrl = request.json['imageUrl']
+      categoryId = request.json['categoryId']
+
 
       new_product = Product(
         title=title,
         description=description,
         creatorUser=creatorUser,
-        imageUrl=imageUrl
+        imageUrl=imageUrl,
+        categoryId=categoryId
     )
       db.session.add(new_product)
       db.session.commit()
@@ -112,10 +134,152 @@ def delete_product(id):
      return product_schema.jsonify(product)
 
 
+@app.route('/getuser', methods = ['GET'])
+def get_users():
+        all_users = User.query.all()
+        users_results = user_schema_many.dump(all_users)
+        return jsonify(users_results)
+
+@app.route('/adduser', methods = ['POST'])
+def add_user():
+      login = request.json['login']
+      password = request.json['password']
+      role = request.json['role']
+      #id = request.json['id']
+
+      new_user = User(
+        login=login,
+        password=password,
+        role=role,
+        #id=id
+    )
+      db.session.add(new_user)
+      db.session.commit()
+      return product_schema.jsonify(new_user)
+
+@app.route('/updateuser/<id>/', methods = ['PUT'])
+def update_user(id):
+      user = User.query.get(id)
+
+      login = request.json['login']
+      password = request.json['password']
+      role = request.json['role']    
+     
+      user.login = login
+      user.password = password
+      user.role = role
+
+      db.session.commit()
+      return user_schema.jsonify(user)
+
+@app.route('/deleteuser/<id>/', methods = ['DELETE'])
+def delete_user(id):
+     user = User.query.get(id)
+     db.session.delete(user)
+     db.session.commit()
+
+     return user_schema.jsonify(user)
+
+
+@app.route('/getcategory', methods = ['GET'])
+def get_categories():
+        all_categories = Category.query.all()
+        categories_results = categories_schema_many.dump(all_categories)
+        return jsonify(categories_results)
+
+@app.route('/addcategory', methods = ['POST'])
+def add_category():
+      #id = request.json['id']
+      name = request.json['name']
+
+      new_category = Category(
+        #id=id,
+        name=name
+    )
+      db.session.add(new_category)
+      db.session.commit()
+      return category_schema.jsonify(new_category)
+
+@app.route('/updatecategory/<id>/', methods = ['PUT'])
+def update_category(id):
+      category = Category.query.get(id)
+
+      #id = request.json['id']
+      name = request.json['name'] 
+     
+      #category.id = id
+      category.name = name
+
+      db.session.commit()
+      return category_schema.jsonify(category)
+
+@app.route('/deletecategory/<id>/', methods = ['DELETE'])
+def delete_category(id):
+     category = Category.query.get(id)
+     db.session.delete(category)
+     db.session.commit()
+
+     return category_schema.jsonify(category)
+
+
+
+@app.route('/getcomment', methods = ['GET'])
+def get_comments():
+        all_comments = Comment.query.all()
+        comments_results = comment_schema_many.dump(all_comments)
+        return jsonify(comments_results)
+
+@app.route('/addcomment', methods = ['POST'])
+def add_comments():
+      #id = request.json['id']
+      productId = request.json['productId']
+      description = request.json['description']
+      isDeleted = request.json['isDeleted']
+      creatorUserId = request.json['creatorUserId']
+
+      new_comment = Comment(
+        productId=productId,
+        description=description,
+        creatorUseIdr=creatorUserId,
+        isDeleted=isDeleted
+    )
+      db.session.add(new_comment)
+      db.session.commit()
+      return comment_schema.jsonify(new_comment)
+
+@app.route('/updatecomment/<id>/', methods = ['PUT'])
+def update_comment(id):
+      comment = Comment.query.get(id)
+
+      #id = request.json['id']
+      productId = request.json['productId']
+      description = request.json['description']
+      isDeleted = request.json['isDeleted']
+      creatorUserId = request.json['creatorUserId']
+     
+      comment.productId=productId,
+      comment.description=description,
+      comment.creatorUseIdr=creatorUserId,
+      comment.isDeleted=isDeleted
+
+      db.session.commit()
+      return comment_schema.jsonify(comment)
+
+@app.route('/deletecomment/<id>/', methods = ['DELETE'])
+def delete_comment(id):
+     comment = Comment.query.get(id)
+     db.session.delete(comment)
+     db.session.commit()
+
+     return comment_schema.jsonify(comment)
+
+
+
+
 if __name__ == "__main__":
     # Utwórz kontekst aplikacji przed utworzeniem bazy danych
     with app.app_context():
         # Tworzenie bazy danych
         db.create_all()
 
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,42 @@
-
 import './App.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-
-  const[products, setProdcuts] = useState([])
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1/get', {
-      'methods':'GET',
+    fetch('http://127.0.0.1:5000/get', {
+      method: 'GET', // Poprawione z 'methods' na 'method'
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     })
-    .then(resp => resp.json())
-    .then(resp => console.log(resp))
-    .catch(error => console.log(error))
-  })
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        setUsers(data); // Tu możesz zaktualizować stan komponentu
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+  }, []); // Dodaj pustą tablicę, aby useEffect wykonywał się tylko raz po zamontowaniu komponentu
+
   return (
     <div className="App">
-     <h1>Moja apka</h1>
+      <h1>Moja apka</h1>
+      {users.map(product => {
+        return (
+          <div key = {product.title}>
+            <h2>{product.description}</h2>
+            <h2>{product.imageUrl}</h2>
+          </div>
+        )
+      })}
+    
     </div>
   );
 }
